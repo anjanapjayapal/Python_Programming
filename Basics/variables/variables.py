70477b4b9d3c960b0664c59b6d04e8a29ec9b0bb
ANJANA P <148357013+anjujayapal@users.noreply.github.com>
2024-09-09 20:55:25 +0530

    variables.py

---
+++ Basics/variables/variables.py
@@ -0,0 +1,24 @@
+# 1. Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
+break = 5    
+print(break)
+# SyntaxError: invalid syntax
+
+
+# 2. Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+year=2003
+curr=2024
+age=curr-year
+print(f"Your age is {age}")
+# Your age is 21
+
+
+# 3. Store your first, middle and last name in three different variables and then print your full name using these variables
+first="anjana"
+last="p"
+name=first+' '+last
+print(f"Your full name is {name}")
+# Your full name is anjana p
+
+
+# 4. Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
+1record, record-one, record^one, continue
