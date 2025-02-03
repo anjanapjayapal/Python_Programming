7b7370b8e83e026a65b669443b44a731d2c4027e
ANJANA P <148357013+anjanapjayapal@users.noreply.github.com>
2025-02-03 20:26:50 +0530

    Add files via upload

--- File Handling/Task46.py
+++ File Handling/Task46.py
@@ -1,1 +1,15 @@
+# TASK 46: Write a function in python to count the number of lines from a text file "story.txt" 
+# which is not starting with an alphabet "T".
+
+count=0
+with open("story.txt",'r') as file:
+    lines=file.readlines()
+    print("Total number of lines in the file: ",len(lines))
+    for i in lines:
+        if not i.startswith('T'):
+            count+=1
+print("Number of lines not starting with T: ",count)
+                
+        
+    
 
